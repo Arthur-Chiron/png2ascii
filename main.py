261a82307712dc62ad759adba3acf60297965473
Arthur-Chiron <Arthur.chiron@outlook.fr>
2022-04-24 18:54:16 +0200

    test

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-def main():
-    print("oui")
-    print("test")
+import argparse
+import cv2
 
 
-if __name__ == '__main__':
-    main()
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", help="path to the image to be converted")
+    return parser
+
+
+def main(args):
+    image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
+
+
+
+if __name__ == "__main__":
+    args = make_parser().parse_args()
+    main(args)
