425f019f30a31cdb6ed9c934cd3e09f78900ad4c
Arthur-Chiron <Arthur.chiron@outlook.fr>
2022-04-24 21:04:20 +0200

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import cv2
+import numpy as np
 
 
 def make_parser():
@@ -9,8 +10,37 @@ def make_parser():
 
 
 def main(args):
-    image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
-
+    img = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        return
+    new_height = img.shape[1]
+    new_width = int(img.shape[1] * 8 / 6)
+    img_resized = cv2.resize(img, (new_height, new_height))
+    for i in range(new_height):
+        for j in range(new_width):
+            pxl_val = img_resized[i][j]
+            pxl_val = pxl_val / 256
+            if 0.0 <= pxl_val < 0.1:
+                print('.', end='')
+            elif 0.1 <= pxl_val < 0.2:
+                print(',', end='')
+            elif 0.2 <= pxl_val < 0.3:
+                print(';', end='')
+            elif 0.3 <= pxl_val < 0.4:
+                print('!', end='')
+            elif 0.4 <= pxl_val < 0.5:
+                print('v', end='')
+            elif 0.5 <= pxl_val < 0.6:
+                print('l', end='')
+            elif 0.6 <= pxl_val < 0.7:
+                print('L', end='')
+            elif 0.7 <= pxl_val < 0.8:
+                print('F', end='')
+            elif 0.8 <= pxl_val < 0.9:
+                print('E', end='')
+            else:
+                print('$', end='')
+        print('')
 
 
 if __name__ == "__main__":
